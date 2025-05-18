271af5c5e1f62ae0cb381ad96b684f3d086b2ba5
AlaniRodriguess <160678699+AlaniRodrigues@users.noreply.github.com>
2025-05-18 12:38:10 -0300

    Update NavbarComponent.vue
    Corrigido a palavra

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="bg-pink-300 text-black py-4 px-8 flex justify-between items-center">
         <h1 class="text-lg font-bold">
-            Portifólio
+            Portfólio
         </h1>
         <ul class="flex gap-6">
             <li><router-link to="/" class="houver:underline">Início</router-link></li>
